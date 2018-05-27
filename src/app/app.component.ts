dae11501891571e0e4103ea09af65778880d342b
Sander Valstar <sander@email.com>
2018-05-27 15:28:45 -0700

    adds firebase with GitHub login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { auth } from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  user = {};
+
+  constructor(public afAuth: AngularFireAuth) {
+  }
+
+
+  ngOnInit(): void {
+    this.afAuth.user.subscribe(u => {
+      this.user = u;
+      console.log('user', u);
+    });
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GithubAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
